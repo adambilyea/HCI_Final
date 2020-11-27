3fc02f5e3073d01777b54b275dc52049be79828b
Adam Bilyea <adam.bilyea@ontariotechu.net>
2020-11-26 22:20:45 -0500

    Networking prelim works

---
+++ HCI Final/Assets/GameSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Photon.Pun;
+
+
+
+public class GameSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatPlayer();
+    }
+
+    private void CreatPlayer()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+    }
+}
---
+++ HCI Final/Assets/QuickStartRoomController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class QuickStartRoomController : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField]
+    private int multiplayerSceneIndex;
+
+    public override void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    public override void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        StartGame();
+        Debug.Log("Joined Room");
+    }    
+
+    private void StartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting Game");
+            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+        }
+    }
+}
